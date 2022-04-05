60a189ddb70398177dfa8771530edc81c6ea651d
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-04-05 10:31:24 +0530

    Time: 131 ms (24.27%), Space: 79.2 MB (42.82%) - LeetHub

--- 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.java
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.java
@@ -1,7 +1,11 @@
 class Solution {
     public int maximumCandies(int[] candies, long k) {
         long ans = 0;
-        long low = 1, high = Long.MAX_VALUE;
+        
+        long low = 1, high = 0;
+        for(int i: candies) {
+            high = Math.max(high, i);
+        }
         while(low<=high){
             long mid = low + (high-low)/2;
             if(possible(candies, k, mid)) {
