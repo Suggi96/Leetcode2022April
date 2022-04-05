c4fc7b8619b2b50ebef431c0621b5af121eeb7f9
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-04-05 11:38:01 +0530

    Time: 19 ms (8.31%), Space: 48 MB (6.88%) - LeetHub

---
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int trap(int[] height) {
+       //min(left[i], right[i]) - a[i] = water trapped at ith index
+        //left[i] -> maximum height of building from i to 0 (left direction)
+        //right[i] -> maximum height of building from i to n-1 (right direction)
+        
+        int n = height.length;
+        int[] prefixMax = new int[n];
+        int[] suffixMax = new int[n];
+        prefixMax[0] = height[0];
+        int maxi = height[0];
+        for(int i=1;i<n;i++) {
+            maxi = Math.max(maxi, height[i]);
+            prefixMax[i] = maxi;
+        }
+        suffixMax[n-1] = height[n-1];
+        maxi = height[n-1];
+        for(int i=n-2;i>=0;i--) {
+            maxi = Math.max(maxi, height[i]);
+            suffixMax[i] = maxi;
+        }
+        // for(int i: prefixMax)
+        //     System.out.print(i+" ");
+        // System.out.println();
+        // for(int i: suffixMax)
+        //     System.out.print(i+" ");
+        System.out.println(n+ " "+prefixMax.length + " "+ suffixMax.length);
+        int ans = 0;
+        for(int i=0;i<n;i++) {
+            ans += Math.min(prefixMax[i],suffixMax[i]) - height[i];
+        }
+        return ans;
+    }
+}
