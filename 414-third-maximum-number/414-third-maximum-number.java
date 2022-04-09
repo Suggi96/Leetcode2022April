22bd1f68f9ab3082066704e36f0ec8aa61bfafc4
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-04-09 11:40:22 +0530

    Time: 1 ms (96.29%), Space: 44 MB (47.20%) - LeetHub

--- 414-third-maximum-number/414-third-maximum-number.java
+++ 414-third-maximum-number/414-third-maximum-number.java
@@ -1,22 +1,22 @@
 class Solution {
     public int thirdMax(int[] nums) {
-        if (nums.length==1) return nums[0];
-        else if(nums.length==2) return Math.max(nums[0], nums[1]);
-        Set<Integer> set = new HashSet<>();
-        for(int i: nums)
-            set.add(i);
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>            
-                                            (Collections.reverseOrder());
-        for(int i: set) 
-            maxHeap.add(i);
-        if(maxHeap.size()<3){
-            return maxHeap.peek();
+        long max = Long.MIN_VALUE, sMax = Long.MIN_VALUE, 
+                   tMax = Long.MIN_VALUE;
+        for (int i : nums) {
+            if (i > max) {
+                tMax = sMax;
+                sMax = max;
+                max = i;
+            } else if (i > sMax && i != max) {
+                tMax = sMax;
+                sMax = i;
+            } else if (i > tMax && i != sMax && i != max) {
+                tMax = i;
+            }
         }
-        int ans = 0;
-        for(int i=0;i<3;i++) {
-            ans = maxHeap.remove();
-         //   System.out.println(ans);
-        }
-        return ans;
+		
+		// if thirdMax was not updated, return the first Max
+		
+        return tMax == Long.MIN_VALUE ? (int) max : (int) tMax;
     }
 }
