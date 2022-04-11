a4259f5c589d0649b3ca8355b041467f50a6935e
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-04-11 19:30:42 +0530

    Time: 4 ms (95.74%), Space: 42.9 MB (92.25%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.java
+++ 1260-shift-2d-grid/1260-shift-2d-grid.java
@@ -1,41 +1,24 @@
 class Solution {
     public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+        List<List<Integer>> ans = new ArrayList<>();
         int m = grid.length;
         int n = grid[0].length;
-        List<Integer> oneD = new ArrayList<>();
-        for(int i=0;i<m;i++) {
-            for(int j=0;j<n;j++) {
-                oneD.add(grid[i][j]);
+        int[][] shiftedGrid = new int[m][n];
+        for(int r=0;r<m;r++) {
+            for(int c=0;c<n;c++) {
+                int newIdx = ((r*n + c) + k) % (m*n); //mapping 2d index to 1d index                                                        after k shifts
+                int newR = newIdx/n; //n is num of cols
+                int newC = newIdx%n; //n is num of cols
+                shiftedGrid[newR][newC] = grid[r][c]; //save curVal in newR newC idx
             }
         }
-            
-        //shift each element in oneD list by k positions and save it in             shifted array
-        //formula = (index + k) % m*n . here m*n=size of 2d grid
-        int[] shifted = new int[m*n]; //creating to prevent overwriting in 
-                                      // oneD list 
-        for(int i=0;i<oneD.size();i++) {
-            int newIndex = (i+k)%(m*n);
-            int curVal = oneD.get(i);
-            shifted[newIndex] = curVal;
-        }
-
-        //Copy 1d array shifted into 2D list res
-        List<List<Integer>> res = new ArrayList<>();
-        List<Integer> row = new ArrayList<>();
-        int c = 0;
-        for(int i=0;i<shifted.length;i++) {
-            
-            if(c<n) {
-                row.add(shifted[i]);
-                c++;
-            }
-            if(c==n) {
-                res.add(new ArrayList<>(row));
-                c=0;
-                row.clear();
-            }
-        }
-        return res;
-        
+       for(int i=0;i<m;i++) {
+           List<Integer> temp = new ArrayList<>();
+           for(int j=0;j<n;j++) {
+            temp.add(shiftedGrid[i][j]);
+           }
+           ans.add(new ArrayList<>(temp));
+    }
+         return ans;
     }
 }
