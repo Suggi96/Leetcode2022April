60a5d042a96aeee0fc905e27cc2533810e967b86
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-04-11 17:26:55 +0530

    Time: 3 ms (12.37%), Space: 42.6 MB (57.97%) - LeetHub

--- 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.java
+++ 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.java
@@ -1,28 +1,28 @@
 class Solution {
+    HashMap<Character, Integer> map = new HashMap<>();
     public boolean isAlienSorted(String[] words, String order) {
-        int[] dict = new int[128];
-        for (int i = 0; i < order.length(); i++) {
-            dict[order.charAt(i)] = i;
+        for(int i=0;i<order.length();i++) {
+            map.put(order.charAt(i), i);
         }
-        for(int i = 1; i < words.length; i++) {
-            String cur = words[i];
-            String pre = words[i - 1];
-            int length = Math.min(cur.length(), pre.length());
-            boolean equal = true;
-            for (int j = 0; j < length; j++) {
-                int curOrder = dict[cur.charAt(j)];
-                int preOrder = dict[pre.charAt(j)];
-                if(curOrder < preOrder) {
-                    return false;
-                } else if (curOrder > preOrder){
-                    equal = false;
-                    break;
-                } else {
-                    continue;
-                }
-            }           
-            if(equal && cur.length() < pre.length()) return false;
+        for(int i=1;i<words.length;i++) {
+            if(compare(words[i-1], words[i]) > 0)
+                return false;
         }
         return true;
+    }
+    public int compare(String first, String second) {
+        int i = 0, j = 0;
+        int compareVal = 0;
+        while(i<first.length() && j<second.length() && compareVal==0) {
+            compareVal = map.get(first.charAt(i)) - map.get(second.charAt(j));
+            i++;
+            j++;
+        }
+        //case to handle [helper, hel]
+        if(compareVal==0) {
+            return first.length() - second.length();
+        }
+        else
+            return compareVal;
     }
 }
