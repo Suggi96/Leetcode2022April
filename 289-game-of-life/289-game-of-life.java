e7c59a05e2419d2009fa5e6d9bae11423f73f285
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-04-12 12:48:44 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (95.68%) - LeetHub

--- 289-game-of-life/289-game-of-life.java
+++ 289-game-of-life/289-game-of-life.java
@@ -1,67 +1,49 @@
 class Solution {
-    
+    //8 directions with respect to cordinates (0,0)
+    private int[][] direction = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}, 
+                                 {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
     public void gameOfLife(int[][] board) {
         int m = board.length;
         int n = board[0].length;
-        Map<String, Integer> map = new HashMap<>();
-        int[][] grid = new int[m][n];
-        for(int r=0;r<m;r++) {
-            for(int c=0;c<n;c++) {
-                int countOne = getCountOne(board, r, c);
-                change(board, grid, countOne, r, c);
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
+                if(board[i][j]==1) {
+                    int activeNeigh = getActiveNeigh(board, i, j);
+                    if(activeNeigh<2 || activeNeigh>3) 
+                        board[i][j] = -2; //intermediate state and also use to count 1s
+                }
+                else {
+                    int activeNeigh = getActiveNeigh(board, i, j);
+                    if(activeNeigh==3)
+                        board[i][j] = 3;
+                }
             }
+            
         }
-        //copy grid values into board
-        for(int i=0;i<m;i++)
-            for(int j=0;j<n;j++)
-                board[i][j] = grid[i][j];
+        updateBoard(board);
         return;
     }
-    private int getCountOne(int[][] board, int r, int c) {
-        int count = 0;
-        if(r>=0 && r<board.length && c+1>=0 && c+1<board[0].length) {
-            if(board[r][c+1]==1) count++;
-        }
-        if(r>=0 && r<board.length && c-1>=0 && c-1<board[0].length) {
-            if(board[r][c-1]==1) count++;
-        }
-        if(r-1>=0 && r-1<board.length && c>=0 && c<board[0].length) {
-            if(board[r-1][c]==1) count++;
-        }
-        if(r+1>=0 && r+1<board.length && c>=0 && c<board[0].length) {
-            if(board[r+1][c]==1) count++;
-        }
-        if(r-1>=0 && r-1<board.length && c-1>=0 && c-1<board[0].length) {
-            if(board[r-1][c-1]==1) count++;
-        }
-        if(r-1>=0 && r-1<board.length && c+1>=0 && c+1<board[0].length) {
-            if(board[r-1][c+1]==1) count++;
-        }
-        if(r+1>=0 && r+1<board.length && c-1>=0 && c-1<board[0].length) {
-            if(board[r+1][c-1]==1) count++;
-        }
-        if(r+1>=0 && r+1<board.length && c+1>=0 && c+1<board[0].length) {
-            if(board[r+1][c+1]==1) count++;
-        }
-        return count;
         
-    }
-    private void change(int[][] board, int[][] grid, int count1, int r, int c) {
-        if(board[r][c]==1 && count1<=1) {
-            grid[r][c] = 0;
-            return;
+        private int getActiveNeigh(int[][] board, int i, int j) {
+            int activeNeigh = 0;
+            for(int[] d: direction) {
+                int r = i + d[0];
+                int c = j + d[1];
+                if(r>=0 && r<board.length && c>=0 && c<board[0].length && 
+                  (board[r][c]==1 || board[r][c]==-2))
+                    activeNeigh++;
+            }
+            return activeNeigh;
         }
-        else if(board[r][c]==1 && (count1==2 || count1==3)) {
-            grid[r][c] = 1;
-            return;
+        
+    private void updateBoard(int[][] board) {
+            for(int i=0;i<board.length;i++) {
+                for(int j=0;j<board[0].length;j++) {
+                    if(board[i][j]==-2)
+                        board[i][j]=0;
+                    else if(board[i][j]==3)
+                        board[i][j] = 1;
+                }
+            }
         }
-        else if(board[r][c]==1 && count1>=4) {
-            grid[r][c] = 0;
-            return;
-        }
-        else if(board[r][c]==0 && count1==3) {
-            grid[r][c] = 1;
-            return;
-        }
-    }
 }
