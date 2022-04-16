71b35e9ad66df31ef2b8089a78f17f004baa7566
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-04-16 11:37:27 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (87.14%) - LeetHub

--- 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.java
+++ 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.java
@@ -16,13 +16,13 @@
 class Solution {
     int sum = 0;
     public TreeNode convertBST(TreeNode root) {
-        if(root==null) return null;
         //reverse Inorder traversal
-        convertBST(root.right);
-        sum += root.val;
-        root.val = sum;
-        convertBST(root.left);
-        
+        if(root!=null) {
+            convertBST(root.right);
+            sum += root.val;
+            root.val = sum;
+            convertBST(root.left);
+        }
         return root;
     }
 }
