d039b1e93fe76b59e54e96cb84054b58aaa63117
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-04-17 15:17:34 +0530

    Time: 1 ms (99.87%), Space: 43 MB (82.87%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -3,7 +3,8 @@ class Solution {
         int l = 0, r = 0;
         int jmp = 0;
         int n = nums.length;
-        while(r<n-1) { 
+        while(r<n-1) { //if r reaches n-1 index or > n-1 index then we found answer
+                       //so break out of the loop
             int far = 0;
             for(int i=l;i<=r;i++) 
                 far = Math.max(far, i+nums[i]);
