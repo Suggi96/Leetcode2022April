2a92c3b84ac8556f1f41222f4d34430de5f1824c
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-04-17 10:01:09 +0530

    Time: 59 ms (66.67%), Space: 39.4 MB (100.00%) - LeetHub

--- 2240-number-of-ways-to-buy-pens-and-pencils/2240-number-of-ways-to-buy-pens-and-pencils.java
+++ 2240-number-of-ways-to-buy-pens-and-pencils/2240-number-of-ways-to-buy-pens-and-pencils.java
@@ -3,13 +3,14 @@ class Solution {
         long ways = 0;
         if(cost1>total && cost2>total)
             return 1;
-        //count 0 ways as 1 way in our ans
+        
         long numOfPens = total/cost1;
         long numOfPencils = 0;
-        for(int i=0;i<=numOfPens;i++) {
+        //start i=0 because we can buy 0 pens also irrespective of cost1
+        for(int i=0;i<=numOfPens;i++) { 
             long moneyLeft = total - (long)i*cost1;
-          //  System.out.println(moneyLeft);
-            numOfPencils += 1 + moneyLeft/cost2;
+            //include 0 ways as 1 way in our ans so we add 1 in each iteration
+            numOfPencils += 1 + moneyLeft/cost2; 
         }
         return numOfPencils;
     }
