43066237696fb568ef9bee4cded68ba46d7e2f1d
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-04-29 20:13:10 +0530

    Time: 0 ms (100.00%), Space: 43.1 MB (80.31%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.java
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -1,33 +1,29 @@
-// -1 -> no color , 0-> green, 1 -> red
 class Solution {
     public boolean isBipartite(int[][] graph) {
-        int len = graph.length; //no of vertices in graph
+        int len = graph.length;
         int[] color = new int[len];
         Arrays.fill(color, -1);
-        
         for(int i=0;i<len;i++) {
             if(color[i]==-1) {
-                color[i] = 1; //color it red
-                if(!dfs(i, graph, color)) { //color all adjacent node of i with diff color
-                    return false;
-                }
+                color[i] = 0; 
             }
+            if(!dfs(i, color, graph))
+                return false;
         }
         return true;
     }
-    private boolean dfs(int index, int[][] graph, int[] color) {
+    private boolean dfs(int index, int[] color, int[][] graph) {
         int currentColor = color[index];
         for(int adjIndex: graph[index]) {
-            if(color[adjIndex] == currentColor) //adj nodes should not same color
+            if(color[adjIndex]==currentColor)
                 return false;
-            
             if(color[adjIndex]==-1) {
-                //if adjNode is uncolored, color it with diff color
-                color[adjIndex] = 1 - currentColor;
-            
-            if(!dfs(adjIndex, graph, color))//recursive dfs might fail when adjnodes have same color
-                return false;
-            } 
+                int newColor = 1 - currentColor;
+                color[adjIndex] = newColor;
+                if(!dfs(adjIndex, color, graph)) {
+                    return false;
+                }
+            }
         }
         return true; //happy case
     }
