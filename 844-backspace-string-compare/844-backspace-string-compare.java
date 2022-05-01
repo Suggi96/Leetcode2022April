af58bc12f310f0055acd95b69b9b62b883c97ccd
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-05-01 12:20:55 +0530

    Time: 2 ms (57.60%), Space: 40.4 MB (86.10%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.java
+++ 844-backspace-string-compare/844-backspace-string-compare.java
@@ -2,29 +2,26 @@ class Solution {
     public boolean backspaceCompare(String s, String t) {
         Stack<Character> st1 = new Stack<>();
         Stack<Character> st2 = new Stack<>();
-        for(int i=0;i<s.length();i++) {
-            if(st1.empty() && s.charAt(i)=='#') continue;
-            if(!st1.empty() && s.charAt(i)=='#')
-                st1.pop();
-            else
-                st1.push(s.charAt(i));
-        }
-        for(int i=0;i<t.length();i++) {
-            if(st2.empty() && t.charAt(i)=='#') continue;
-            if(!st2.empty() && t.charAt(i)=='#')
-                st2.pop();
-            else
-                st2.push(t.charAt(i));
-        }
-        StringBuilder x = new StringBuilder();
-        StringBuilder y = new StringBuilder();
+        build(s, st1);
+        build(t, st2);
+        StringBuilder ans1 = new StringBuilder();
+        StringBuilder ans2 = new StringBuilder();
         for(char c: st1) {
-         //   System.out.print(c);
-            x.append(c);
+            ans1.append(c);
         }
-           
-        for(char c: st2) 
-            y.append(c);
-        return x.compareTo(y) == 0;
+        for(char c: st2) {
+            ans2.append(c);
+        }
+        return ans1.toString().equals(ans2.toString());
+    }
+    private void build(String s, Stack<Character> st) {
+        for(int i=0;i<s.length();i++) {
+            if(st.isEmpty() && s.charAt(i)=='#') 
+                continue;
+            if(!st.isEmpty() && s.charAt(i)=='#')
+                st.pop();
+            else
+                st.push(s.charAt(i));
+        }
     }
 }
