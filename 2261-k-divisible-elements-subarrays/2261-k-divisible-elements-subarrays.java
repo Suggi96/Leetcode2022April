3eff38a0e77732c8d3b084ee9f7a467eee29838b
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-05-02 21:04:43 +0530

    Time: 384 ms (40.00%), Space: 120.9 MB (20.00%) - LeetHub

--- 2261-k-divisible-elements-subarrays/2261-k-divisible-elements-subarrays.java
+++ 2261-k-divisible-elements-subarrays/2261-k-divisible-elements-subarrays.java
@@ -1,34 +1,27 @@
 class Solution {
     public int countDistinct(int[] nums, int k, int p) {
-        Set<List<Integer>> set = new HashSet<>();
-        int n = nums.length;
-        for(int i=0;i<n;i++) {
-            for(int j=i;j<n;j++) {
-                boolean validSub = compute(nums, i, j, k, p);
-                if(validSub) {
-                    List<Integer> list = genSub(nums, i, j);
-                    set.add(list);
-                }
-            }
-        }
-        return set.size();
+        HashSet<String> hs = new HashSet<>();
+
+		for(int i=0; i<nums.length; ++i) {
+			int cnt = 0;
+			StringBuilder sb = new StringBuilder();
+
+			for(int j=i; j<nums.length; ++j) {
+
+				if(nums[j]%p == 0) {
+					cnt++;
+				}
+
+				if(cnt > k) {
+					break;
+				}
+
+				sb.append(nums[j] + ",");
+
+				hs.add(sb.toString());
+			}
+		}
+
+		return hs.size();
     }
-    private List<Integer> genSub(int[] nums, int start, int end) {
-        List<Integer> list = new ArrayList<>();
-        for(int i=start;i<=end;i++) {
-            list.add(nums[i]);
-        }
-        return list;
-    }
-    private boolean compute(int[] nums, int start, int end, int k, int p) {
-        int count = 0;
-        for(int i=start;i<=end;i++) {
-            if(nums[i]%p==0) {
-                count++;
-            }
-        }
-        if(count<=k)
-            return true;
-        return false;
-    }
-} 
+}
