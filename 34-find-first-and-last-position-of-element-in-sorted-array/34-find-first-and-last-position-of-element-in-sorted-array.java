8af50bc2598f61ee6fca24ad8785f04560178461
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-07-25 10:31:47 +0530

    Time: 0 ms (100.00%), Space: 48.2 MB (6.08%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,18 +1,21 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int first = firstOccur(nums, target);
-        int last = lastOccur(nums, target);
+        int first = find(nums, target, false);
+        int last = find(nums, target, true);
         int[] ans = new int[] {first, last};
         return ans;
     }
-    private int firstOccur(int[] nums, int target) {
+    private int find(int[] nums, int target, boolean lastOccur) {
         int low = 0, high = nums.length-1;
         int ind = -1;
         while(low<=high) {
             int mid = low + (high-low)/2;
             if(nums[mid]==target) {
                 ind = mid;
-                high = mid - 1; //search in left halft because first occur lies in left part
+                if(!lastOccur)
+                    high = mid - 1; //search in left half because first occur lies in left part                                      
+                else
+                    low = mid + 1; //search in right half because last occur lies in right part
             }
             else if(nums[mid]>target) 
                 high = mid - 1;
@@ -21,20 +24,5 @@ class Solution {
         }
         return ind;
     }
-        private int lastOccur(int[] nums, int target) {
-        int low = 0, high = nums.length-1;
-        int ind = -1;
-        while(low<=high) {
-            int mid = low + (high-low)/2;
-            if(nums[mid]==target) {
-                ind = mid;
-                low = mid + 1; //search in left halft because first occur lies in left part
-            }
-            else if(nums[mid]>target) 
-                high = mid - 1;
-            else
-                low = mid + 1;
-        }
-        return ind;
-    }
+
 }
