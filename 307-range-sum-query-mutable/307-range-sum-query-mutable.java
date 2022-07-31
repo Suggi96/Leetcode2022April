73b8b52a92446abae7624cbe62f1686dc6f3b9f8
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-07-31 11:17:05 +0530

    Time: 119 ms (75.25%), Space: 72.3 MB (91.00%) - LeetHub

--- 307-range-sum-query-mutable/307-range-sum-query-mutable.java
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.java
@@ -74,10 +74,3 @@ public class NumArray {
         }
     }
 }
-
-/**
- * Your NumArray object will be instantiated and called as such:
- * NumArray obj = new NumArray(nums);
- * obj.update(index,val);
- * int param_2 = obj.sumRange(left,right);
- */
