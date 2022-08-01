87e424a70f3ae7010420cc22159387c0f0bdaca7
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-08-01 11:37:09 +0530

    Time: 3 ms (33.33%), Space: 41.7 MB (55.56%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.java
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.java
@@ -1,28 +1,9 @@
 class Solution {
     public int minimumOperations(int[] nums) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> a-b);
-        int count = 0;
-        for(int i: nums) 
-            if(i!=0)
-                pq.add(i);
-        
-        while(pq.size()!=0) {
-            int top = pq.remove();
-            setArray(nums, top, pq);
-            count++;
-   
-        }
-        return count;
-    }
-
-    private void setArray(int[] nums, int top, PriorityQueue<Integer> pq) {
-        for(int i=0;i<nums.length;i++) {
-            if(nums[i]!=0)
-                nums[i] = nums[i] - top;
-        }
-        pq.clear();
-        for(int i: nums)
-            if(i!=0)
-                pq.add(i);
+        Set<Integer> set = new HashSet<>();
+        for (int a: nums)
+            if (a > 0)
+                set.add(a);
+        return set.size();
     }
 }
