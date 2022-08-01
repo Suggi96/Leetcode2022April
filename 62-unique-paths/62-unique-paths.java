6283aa069c852c7eda13578e364c874947e526d4
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-08-01 08:50:45 +0530

    Time: 1 ms (55.39%), Space: 40.5 MB (76.55%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,25 +1,25 @@
 class Solution {
     int[][] directions = {{0,1}, {1,0}};
     public int uniquePaths(int m, int n) {
-        return dfs(0, 0, m, n, new HashMap<String, Integer>());
+        int[][] dp = new int[m][n];
+        return dfs(0, 0, m, n, dp);
     }
-    private int dfs(int row, int col, int m, int n, HashMap<String, Integer> memo) {
+    private int dfs(int row, int col, int m, int n,int[][] dp) {
         if(row<0 || row>=m || col<0 || col>=n)
             return 0;
         if(row==m-1 && col==n-1)
             return 1;
-        String curKey = row + "-" + col;
-        if(memo.containsKey(curKey))
-            return memo.get(curKey);
+        if(dp[row][col]!=0)
+            return dp[row][col];
         
         int ans = 0;
         
         for(int[] d: directions) {
             int nrow = row + d[0];
             int ncol = col + d[1];
-            ans += dfs(nrow, ncol, m, n, memo);
+            ans += dfs(nrow, ncol, m, n, dp);
         }
-        memo.put(curKey, ans);
+        dp[row][col] = ans;
         return ans;
     }
 }
