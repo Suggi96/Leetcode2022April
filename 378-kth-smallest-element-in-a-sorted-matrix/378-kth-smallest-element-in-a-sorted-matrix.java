b03557f0379fb07b30ed6124624a10bb8d31d2db
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-08-02 11:51:52 +0530

    Time: 0 ms (100.00%), Space: 56.9 MB (52.97%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,14 +1,29 @@
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-        int n = matrix.length;
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> a-b);
-        for(int i=0;i<n;i++) 
-            for(int j=0;j<n;j++) 
-                pq.add(matrix[i][j]);
-        int ans = 0;
-        for(int i=0;i<k;i++)
-            ans = pq.remove();
-        
-        return ans;
+        int n = matrix.length, lo = matrix[0][0], hi = matrix[n-1][n-1];
+        while(lo<hi) {
+            int mid = lo + (hi-lo)/2;
+            int count = lessEqual(matrix, mid);
+            if(count<k) lo = mid+1;
+            else hi = mid;
+        }
+        return lo;
+    }
+    
+    //This returns the exact poisition where target must be present when 2D array is 
+    //converted to 1D array linearly sorted. If target is present in matrix it return pos, otherwise      pos+1
+    public int lessEqual(int[][] matrix, int target) {
+        int count = 0, n = matrix.length;
+        int i = n-1, j = 0;
+        while(i>=0 && j<n) {
+            if(matrix[i][j]>target)
+                i--;
+            
+            else { //matrix[i][j]<=target 
+                count = count + i + 1; //0 based indexing so we add +1
+                j++;
+            } 
+        }
+        return count;
     }
 }
