467d8881b582105929cc346838b87ff5b4bd1378
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-08-02 11:22:16 +0530

    Time: 52 ms (6.27%), Space: 56.8 MB (56.83%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,21 +1,14 @@
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-        //max heap
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b-a);
         int n = matrix.length;
-        for(int i=0;i<n;i++) {
-            for(int j=0;j<n;j++) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> a-b);
+        for(int i=0;i<n;i++) 
+            for(int j=0;j<n;j++) 
                 pq.add(matrix[i][j]);
-            }
-        }
-        //pop (totalsize-k+1) times as we are using maxheap
-        int count = (n*n-k+1);
-        System.out.print(count);
         int ans = 0;
-        while(count>0) {
+        for(int i=0;i<k;i++)
             ans = pq.remove();
-            count--;
-        }
+        
         return ans;
     }
 }
