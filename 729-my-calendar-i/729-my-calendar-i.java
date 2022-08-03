f76ccc13d5880c827242eb17198d5f70029fd8fa
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-08-03 11:06:07 +0530

    Time: 27 ms (83.69%), Space: 54.8 MB (35.87%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.java
+++ 729-my-calendar-i/729-my-calendar-i.java
@@ -1,14 +1,30 @@
 class MyCalendar {
-    private List<int[]> books = null;
+    private List<int[]> bookings = null;
     public MyCalendar() {
-        books = new ArrayList<>();
+        bookings = new ArrayList<>();
     }
     
     public boolean book(int start, int end) {
-        for(int[] b: books) 
-            if (Math.max(b[0], start) < Math.min(b[1], end)) return false;
-        books.add(new int[] {start, end});
-        return true;
+        int s=0, e=bookings.size()-1;
+        while (s<=e){
+            int mid=s+(e-s)/2;
+            int[] bk=bookings.get(mid);
+            if (bk[0]==start){
+                return false;
+            }else if (bk[0]>start){
+                e=mid-1;
+            }else{
+                s=mid+1;
+            }
+        }
+        if (s>0 && start<bookings.get(s-1)[1])
+            return false;
+
+        if (s<bookings.size() && end>bookings.get(s)[0])
+            return false;
+        
+        bookings.add(s, new int[]{start, end});
+        return true; 
     }
 }
 
