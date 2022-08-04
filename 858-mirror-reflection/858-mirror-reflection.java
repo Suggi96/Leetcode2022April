c0c3bd5f8e2b0307913bd11158e14a060d2b4f9c
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-08-04 13:19:59 +0530

    Time: 1 ms (33.33%), Space: 40.6 MB (75.00%) - LeetHub

--- 858-mirror-reflection/858-mirror-reflection.java
+++ 858-mirror-reflection/858-mirror-reflection.java
@@ -1,15 +1,18 @@
 class Solution {
     public int mirrorReflection(int p, int q) {
-                while (p % 2 == 0 && q % 2 == 0) {
-            p /= 2;
-            q /= 2;
+        // p * m = q * n
+        int m = 1, n = 1;
+        while(p*m!=q*n) {
+            n = n+1;
+            m = (q * n)/p;
         }
-        if (p % 2 == 0) {
+        
+        if(n%2==0)
             return 2;
-        } else if (q % 2 == 0) {
-            return 0;
-        } else {
+        
+        else if(m%2==1) //n is definitely odd and m is even
             return 1;
-        }
+        else            //n is definitely odd and m is odd too
+            return 0;
     }
 }
