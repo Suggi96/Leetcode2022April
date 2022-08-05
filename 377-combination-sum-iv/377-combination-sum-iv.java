b5667663c0fb0659a98e659dbeb4edeac3c07933
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-08-06 00:00:24 +0530

    Time: 3 ms (36.49%), Space: 42 MB (21.92%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.java
+++ 377-combination-sum-iv/377-combination-sum-iv.java
@@ -2,23 +2,19 @@ class Solution {
     public int combinationSum4(int[] nums, int target) {
         return helper(nums, target, new HashMap<Integer, Integer>());
     }
-    private int helper(int[] nums, int target, HashMap<Integer, Integer> memo) {
+    private int helper(int[] nums, int target, Map<Integer, Integer> memo) {
         if(target==0)
             return 1;
-        
         int key = target;
-        
         if(memo.containsKey(key))
             return memo.get(key);
         
         int res = 0;
-        for(int i: nums) {
-            if(i<=target) {
-                res += helper(nums, target-i, memo);
-            }
+        for(int i=0;i<nums.length;i++) {
+            if(nums[i]<=target)
+            res += helper(nums, target-nums[i], memo);
         }
-        
         memo.put(key, res);
-        return memo.get(key);
+        return res;
     }
 }
