4e6bcd3f39a797270898637673f03c02cccc7758
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-08-06 13:32:08 +0530

    Time: 386 ms (26.33%), Space: 98.7 MB (23.71%) - LeetHub

--- 127-word-ladder/127-word-ladder.java
+++ 127-word-ladder/127-word-ladder.java
@@ -1,33 +1,40 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        Set<String> set = new HashSet<>();
-        for(String s: wordList)
-            set.add(s);
-        if(!set.contains(endWord)) return 0;
+         Integer minCount = 0;
         
-        Queue<Pair<String, Integer>> q = new LinkedList<>();
-        q.add(new Pair<String, Integer>(beginWord, 1));
-        while(!q.isEmpty()) {
-            Pair<String, Integer> p = q.remove();
-            StringBuilder word = new StringBuilder(p.getKey());
-            int ladder = p.getValue();
-            if(word.toString().equals(endWord)) {
-               // System.out.println("yes");
-                return ladder;
-            }
+        Set<String> set = new HashSet<>(wordList);
+        
+        Queue<String> q = new LinkedList<>();
+        
+        q.add(beginWord);
+        
+        while (!q.isEmpty()){
+            
+            int size = q.size();
+            minCount++;
+            
+            for (int k=0; k<size; k++){
+                String latestWord = q.poll();
                 
-            set.remove(word.toString());
-            for(int j=0;j<word.length();j++) {
-                char c = word.charAt(j);
-                for(int k=0;k<26;k++) {
-                    char temp = (char)('a' + k);
-                    word.setCharAt(j, temp);
-                    if(set.contains(word.toString()))
-                        q.add(new Pair<>(word.toString(), ladder+1));
+                if (latestWord.equals(endWord))
+                    return minCount;
+                                
+                for (int i=0; i<latestWord.length(); i++){
+    
+                    String s1 = latestWord.substring(0,i);   
+                    String s2 = latestWord.substring(i+1,latestWord.length());
+
+                    for (int j=0; j<26; j++){
+                        char temp = (char) ((int)'a'+j);
+                        String s3 = s1+temp+s2;
+                        if (set.contains(s3)){
+                            set.remove(s3);
+                            q.add(s3);
+                        }
+                    }
                 }
-                word.setCharAt(j, c);
-            }
-        }
+            } 
+        }     
         return 0;
     }
 }
