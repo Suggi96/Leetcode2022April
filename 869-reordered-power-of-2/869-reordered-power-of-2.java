6bfade26fc7447efc33dbefc6b563a072871a137
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-08-26 19:13:51 +0530

    Time: 1 ms (100.00%), Space: 40.5 MB (82.67%) - LeetHub

---
+++ 869-reordered-power-of-2/869-reordered-power-of-2.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean reorderedPowerOf2(int n) {
+        int[] counter = count(n);
+        int num = 1;
+        for(int i=0;i<31;i++) {
+            if(Arrays.equals(counter, count(num)))
+                return true;
+            num = num<<1;
+        }
+        return false;
+    }
+    private int[] count(int n) {
+        int[] arr = new int[10];
+        while(n>0) {
+            int rem = n % 10;
+            arr[rem]++;
+            n /= 10;
+        }
+        return arr;
+    }
+}
