7e0d30daafa4c50143eee51713218b6716a05621
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-09-10 11:06:33 +0530

    Time: 9 ms (38.09%), Space: 43.5 MB (27.96%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,37 +1,30 @@
 class Solution {
     public int maxProfit(int k, int[] prices) {
-        int canBuy = 1; //buy only if its 1 otherwise its 0 so sell
-        int transactionCount = k; // i am allowed to make k transacn in this problem
-        int idx = 0; //we start at index 0
-        return maxprofit(idx, canBuy, transactionCount, prices, 
-                         new HashMap<String,Integer>());
+        int n = prices.length;
+        int[][] dp = new int[n][2*k];
+        for(int[] arr: dp)
+            Arrays.fill(arr, -1);
+        return f(0, 0, prices, n, k, dp);
     }
-    private int maxprofit(int idx, int canBuy, int transactionCount, int[] prices,
-                         HashMap<String, Integer> memo) {
-        //base case when idx>=prices.length or transactionCount is 0 
-        //I return 0 because impossible to make money when i dont have transaction count          // or reached index which isnt present in prices array
-        if(idx>=prices.length || transactionCount==0)
+    private int f(int idx, int transNo, int[] prices, int n, int k, int[][] dp) {
+        if(idx>=n || transNo==2*k)
             return 0;
-        
+        if(dp[idx][transNo]!=-1)
+            return dp[idx][transNo];
         int ans = 0;
-        String curKey = idx + "-" + canBuy + "-" + transactionCount;
-        
-        if(memo.containsKey(curKey))
-            return memo.get(curKey);
-        //always move to next idx no matter what 
-        if(canBuy==1) {
-            int idle = maxprofit(idx+1, canBuy, transactionCount, prices, memo);
-            int buy = -prices[idx] + maxprofit(idx+1, 0, transactionCount, prices,                                                    memo);
-                        //-prices because i am at loss when i buy a stock initially
-            ans = Math.max(idle, buy);
-        }
+        if(transNo%2==0) {
+            int buy = -prices[idx] + f(idx+1, transNo+1, prices, n, k, dp);
+            int idle = f(idx+1, transNo, prices, n, k, dp);
+            ans = Math.max(buy, idle);
+            dp[idx][transNo] = ans;
+            return ans;
+        } 
         else {
-            int idle = maxprofit(idx+1, canBuy, transactionCount, prices, memo);
-            int sell = prices[idx] + maxprofit(idx+1, 1, transactionCount-1, prices,                                                  memo);
-            //we subtracted transactionCount - 1 indicating transaction is done
-            ans = Math.max(idle, sell);
+            int sell = prices[idx] + f(idx+1, transNo+1, prices, n, k, dp);
+            int idle = f(idx+1, transNo, prices, n, k, dp);
+            ans = Math.max(sell, idle);
+            dp[idx][transNo] = ans;
+            return ans;
         }
-        memo.put(curKey, ans);
-        return memo.get(curKey);
     }
 }
