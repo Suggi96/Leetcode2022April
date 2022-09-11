1ef46c62c7df5f90cd6474b9650db0a708efc99d
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-09-11 19:36:41 +0530

    Time: 104 ms (24.67%), Space: 71.7 MB (15.67%) - LeetHub

--- 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.java
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.java
@@ -1,24 +1,25 @@
 class Solution {
     public int maxPerformance(int n, int[] speed, int[] efficiency, int k) {
-        int mod = (int)(Math.pow(10, 9) + 7);
         List<int[]> list = new ArrayList<>();
         for(int i=0;i<n;i++) {
             list.add(new int[] {speed[i], efficiency[i]});
         }
+        //sort eff in desc order because we need to maximise performance
+        // performance =  {sum(speed) * MIN(effi) } -> need max performace upto k selections
+        Collections.sort(list, (a, b) -> b[1]-a[1]);
+        //used to pop efficiency upto k
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        //sort list in descending order of their efficiency
-        Collections.sort(list, (a, b)->b[1]-a[1]);
-        long res = 0, Totalspeed = 0;
+        long res = 0, tspeed = 0;
         for(int i=0;i<n;i++) {
-            int curSpeed = list.get(i)[0];
-            int curEfficiency = list.get(i)[1];
+            int cspeed = list.get(i)[0];
+            int ceff = list.get(i)[1];
             if(minHeap.size()==k) {
-                Totalspeed -= minHeap.remove();
+                tspeed -= minHeap.remove();
             }
-            Totalspeed += curSpeed;
-            minHeap.add(curSpeed);
-            res = Math.max(res, curEfficiency*Totalspeed);
+            minHeap.add(cspeed);
+            tspeed += cspeed;
+            res = Math.max(res, tspeed*ceff);
         }
-        return (int) (res % (long)(1e9 + 7));
+        return (int)(res % (long)(1e9 +7));
     }
 }
