8ed2da09a0fbac46f02b25ec084c2e6412cc32ad
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-09-11 19:37:23 +0530

    Time: 50 ms (95.67%), Space: 53.7 MB (98.00%) - LeetHub

--- 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.java
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.java
@@ -7,7 +7,7 @@ class Solution {
         //sort eff in desc order because we need to maximise performance
         // performance =  {sum(speed) * MIN(effi) } -> need max performace upto k selections
         Collections.sort(list, (a, b) -> b[1]-a[1]);
-        //used to pop efficiency upto k
+        //used to store cspeed upto k engineers
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
         long res = 0, tspeed = 0;
         for(int i=0;i<n;i++) {
