9598884ccf3adf7d00b0653afa767419a502bc27
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-10-01 18:24:33 +0530

    Time: 0 ms (100.00%), Space: 47.2 MB (58.87%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,26 +1,30 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int first = find(nums, target, false);
-        int last = find(nums, target, true);
+        int first = find(nums, target, true);
+        if(first==-1)
+            return new int[] {-1, -1};
+        int last = find(nums, target, false);
         int[] ans = new int[] {first, last};
         return ans;
     }
-    private int find(int[] nums, int target, boolean lastOccur) {
-        int low = 0, high = nums.length-1;
+    private int find(int[] arr, int target, boolean firstOccur) {
+        int low = 0, high = arr.length-1;
         int ind = -1;
         while(low<=high) {
-            int mid = low + (high-low)/2;
-            if(nums[mid]==target) {
+            int mid = (low+high)/2;
+            if(arr[mid]==target) {
                 ind = mid;
-                if(!lastOccur)
-                    high = mid - 1; //search in left half because first occur lies in left part                                      
+                if(firstOccur==true) 
+                    high = mid - 1; //first occur lies in left half
                 else
-                    low = mid + 1; //search in right half because last occur lies in right part
+                    low = mid + 1; //;ast occur lies in right half
             }
-            else if(nums[mid]>target) 
+            else if(arr[mid]>target) {
                 high = mid - 1;
-            else
+            }
+            else {
                 low = mid + 1;
+            }
         }
         return ind;
     }
